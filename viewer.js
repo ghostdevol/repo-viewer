8667c9b4b7903609e21bd87d555f48d103681d63
Ghostdevol <karmastudiosbeatz@gmail.com>
2026-03-05 04:01:14 -0600

    Add files via upload

---
+++ viewer.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>Repo Viewer</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h1>Repository HTML Viewer</h1>
+
+<div id="fileList"></div>
+
+<iframe id="viewer"></iframe>
+
+<script src="viewer.js"></script>
+
+</body>
+
+</html>
